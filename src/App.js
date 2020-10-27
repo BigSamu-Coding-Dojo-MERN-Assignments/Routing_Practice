f0e4826283c4cdea7280245b240d5fee47cd6043
Samuel <samuel@MacBook-Pro-de-Samuel.local>
2020-10-27 21:20:21 +0000

    Submitting 'Routing Practice' Assignmnet

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Router } from '@reach/router';
+
+function App() {
+  return (
+    <div className="App bg-light p-3">
+      
+    </div>
+  );
+}
+
+export default App;
